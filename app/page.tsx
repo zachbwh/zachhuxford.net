0a411342808ec2f27dc41d87fd468405abc2d4b9
zachbwh <zachbwh@gmail.com>
2025-03-29 13:15:02 +0000

    poc: next app with panda css

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,31 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
+import { css } from "../styled-system/css";
+import { center } from "../styled-system/patterns";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className={css({ textStyle: "body" })}>
+      <p>It's me!</p>
+      <h1 className={css({ textStyle: "display" })}>
+        Zach Huxford<span>.</span>
+      </h1>
+      <p>
+        I'm a Software Developer from New Zealand. I care about my work having a
+        positive impact on other people, which is why it is important to me that
+        my work has a strong human component, whether it is by creating
+        something useful or something beautiful.
+      </p>
+      <p>
+        I like to spend my spare time browsing wikipedia, reading, listening to
+        podcasts and music, and cooking. I'm particularly interested in full
+        stack web development, graphic design and Linux.
+      </p>
+      <p>
+        I care about my work having a positive impact on other people, which is
+        why it is important to me that my work has a strong human component,
+        whether it is by creating something useful or something beautiful.
+      </p>
     </div>
   );
 }
---
+++ panda.config.ts
@@ -0,0 +1,57 @@
+import { defineConfig, defineTextStyles } from "@pandacss/dev";
+import tokens from "./design-tokens/build/js/tokens";
+
+const typographyTokens = Object.entries(tokens.text.typography);
+
+type PandaTextStyle = {
+  value: {
+    fontFamily: string;
+    fontWeight: string;
+    fontSize: string;
+    lineHeight: string;
+  };
+};
+
+const textStyleDefinitions = typographyTokens.reduce(
+  (acc, [name, designToken]) => {
+    const designTokenValue = designToken.$value;
+    const pandaTextStyle = {
+      value: {
+        fontFamily: designTokenValue.fontFamily,
+        fontWeight: designTokenValue.fontWeight,
+        fontSize: designTokenValue.fontSize,
+        lineHeight: designTokenValue.lineHeight,
+      },
+    };
+
+    acc[name] = pandaTextStyle;
+
+    return acc;
+  },
+  {} as Record<string, PandaTextStyle>
+);
+
+console.log("textStyleDefinitions", textStyleDefinitions);
+
+const textStyles = defineTextStyles(textStyleDefinitions);
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {
+      textStyles,
+    },
+  },
+
+  // The output directory for your css system
+  outdir: "styled-system",
+});
