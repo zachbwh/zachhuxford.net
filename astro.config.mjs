f8300fcfa6a558db0dacbf22cdfa4e9d4bf167da
Zach Huxford <zachbwh@gmail.com>
2026-04-12 19:18:40 +0100

    improvement: fix og tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  site: "https://zachhuxford.net",
   integrations: [mdx()],
   output: "static",
   prefetch: {
